467a3a474039a9213dd6eb1472b5867cf839dc1f
DavidESmith-net <kungmoo79@gmail.com>
2026-07-14 01:53:16 -0500

    Refactor static travel page to dynamic MVC architecture using Handlebars

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = 3000;
 
+// View engine setup for Handlebars
+app.set('views', path.join(__dirname, 'app_server', 'views'));
+app.set('view engine', 'hbs');
+
 // Tell Express to serve static files from the 'public' folder
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public'))); 
+
+// Register the new traveler route
+const travelRouter = require('./app_server/routes/travel');
+app.use('/travel', travelRouter);
 
 // Start the server
 app.listen(PORT, () => {
