b465aec129802247bf39730a9582b208d0e9970b
DavidESmith-net <kungmoo79@gmail.com>
2026-08-16 11:12:53 -0500

    Completed Module 7: Secured application with JWT and HTTP interceptors

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const path = require('path');
+require('dotenv').config();
 require('./app_api/models/db');
+const passport = require('passport');
+require('./app_api/config/passport');
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -12,6 +15,7 @@ app.set('view engine', 'hbs');
 
 // Tell Express to serve static files from the 'public' folder
 app.use(express.static(path.join(__dirname, 'public'))); 
+app.use(passport.initialize());
 
 // Register the new traveler route
 const travelRouter = require('./app_server/routes/travel');
@@ -22,6 +26,7 @@ app.use('/api', (req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   next();
 });
 
@@ -32,4 +37,10 @@ app.use('/api', apiRouter);
 // Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+app.use((err, req, res, next) => {
+  if(err.name === 'UnauthorizedError') {
+    res.status(401).json({"message": err.name + ": " + err.message});
+  }
 });
